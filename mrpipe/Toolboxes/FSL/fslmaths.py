289c37ea5fc5b72ab4d0befc316df2c9171be594
unknown <44135911+jadenecke@users.noreply.github.com>
2024-03-20 22:55:21 +0100

    binarize function

---
+++ mrpipe/Toolboxes/FSL/fslmaths.py
@@ -0,0 +1,22 @@
+from mrpipe.Toolboxes.Task import Task
+import mrpipe.Toolboxes.submodules.hdbet as hdb
+import os
+import mrpipe.Toolboxes
+class Binarize(Task):
+
+    def __init__(self, infile, output, threshold: float, name: str = "binarize", clobber=False):
+        super().__init__(name=name, clobber=clobber)
+        self.inputImage = infile
+        self.threshold = threshold
+        self.outputImage = output
+
+        #add input and output images
+        self.addInFiles([self.inputImage])
+        self.addOutFiles([self.outputImage])
+
+    def getCommand(self):
+        command = f"fslmaths {self.inputImage} -thr {self.threshold} -bin {self.outputImage}"
+        return command
+
+
+
