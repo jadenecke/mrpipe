f6e58fe7851de4adec8e77fbf61679939ca3fc04
jadenecke <44135911+jadenecke@users.noreply.github.com>
2025-01-17 17:27:20 +0100

    added hammers_mith and JHU-DTI atlas

--- mrpipe/modalityModules/PathDicts/Templates.py
+++ mrpipe/modalityModules/PathDicts/Templates.py
@@ -13,7 +13,9 @@ class Templates(PathCollection):
         self.masks = Path(os.path.join(Helper.get_libpath(), os.pardir, "data", "masks"),
                             isDirectory=True, shouldExist=True)
 
-        #MNI
+        # TODO: easy win: Introduce three subclases, i.e. standards / atlasses / masks
+
+        # MNI
         self.mni152_1mm = self.standard.join("MNI152_T1_1mm.nii.gz", shouldExist=True)
         self.mni152_brain_1mm = self.standard.join("MNI152_T1_1mm_brain.nii.gz", shouldExist=True)
         self.mni152_brain_mask_1mm = self.standard.join("MNI152_T1_1mm_brain_mask.nii.gz", shouldExist=True)
@@ -28,7 +30,7 @@ class Templates(PathCollection):
         self.mni152_brain_3mm = self.standard.join("MNI152_T1_3mm_brain.nii.gz", shouldExist=True)
         self.mni152_brain_mask_3mm = self.standard.join("MNI152_T1_3mm_brain_mask.nii.gz", shouldExist=True)
 
-        #Schaefer Atlas 2018
+        # Schaefer Atlas 2018
         self.schaefer2018Dir = self.atlases.join("schaefer2018", isDirectory=True, shouldExist=True)
         self.Schaefer2018_200Parcels_7Networks_order_FSLMNI152_1mm = self.schaefer2018Dir.join("Schaefer2018_200Parcels_7Networks_order_FSLMNI152_1mm.nii.gz", shouldExist=True)
         self.Schaefer2018_200Parcels_7Networks_order_FSLMNI152_1p5mm_CAT12 = self.schaefer2018Dir.join("Schaefer2018_200Parcels_7Networks_order_FSLMNI152_1p5mm_CAT12.nii.gz", shouldExist=True)
@@ -47,7 +49,7 @@ class Templates(PathCollection):
         self.Schaefer2018_400Parcels_17Networks_order_FSLMNI152_2mm = self.schaefer2018Dir.join("Schaefer2018_400Parcels_17Networks_order_FSLMNI152_2mm.nii.gz", shouldExist=True)
         self.Schaefer2018_400Parcels_17Networks_order = self.schaefer2018Dir.join("Schaefer2018_400Parcels_17Networks_order.lut", shouldExist=True)
 
-        #Mindboggle101 Atlas V2
+        # Mindboggle101 Atlas V2
         self.mindboggleDir = self.atlases.join("mindboggle101", isDirectory=True, shouldExist=True)
         self.Braak_stage_Mindboggle_ROIs_DK40 = self.mindboggleDir.join("Braak_stage_Mindboggle_ROIs_DK40.csv", shouldExist=True)
         self.labels_all = self.mindboggleDir.join("labels_all.csv", shouldExist=True)
@@ -55,6 +57,21 @@ class Templates(PathCollection):
         self.OASIS_TRT_20_jointfusion_DKT31_CMA_labels_in_MNI152_1p5mm_v2 = self.mindboggleDir.join("OASIS-TRT-20_jointfusion_DKT31_CMA_labels_in_MNI152_1p5mm_v2.nii.gz", shouldExist=True)
         self.OASIS_TRT_20_jointfusion_DKT31_CMA_labels_in_MNI152_2mm_v2 = self.mindboggleDir.join("OASIS-TRT-20_jointfusion_DKT31_CMA_labels_in_MNI152_2mm_v2.nii.gz", shouldExist=True)
         self.OASIS_TRT_20_jointfusion_DKT31_CMA_labels_in_MNI152_v2 = self.mindboggleDir.join("OASIS-TRT-20_jointfusion_DKT31_CMA_labels_in_MNI152_v2.nii.gz", shouldExist=True)
+
+        # JHU Atlases
+        self.JHUDir = self.atlases.join("JHU_DTI", isDirectory=True, shouldExist=True)
+        self.JHULabels = self.JHUDir.join("JHU-ICBM-labels-atlas.txt", shouldExist=True)
+        self.JHU_1mm = self.JHUDir.join("JHU-ICBM-labels-1mm.nii.gz", shouldExist=True)
+        self.JHU_2mm = self.JHUDir.join("JHU-ICBM-labels-2mm.nii.gz", shouldExist=True)
+
+
+        # Hammersmith (lobar)
+        # JHU Atlases
+        self.HammersmithDir = self.atlases.join("LobarHammersmith", isDirectory=True, shouldExist=True)
+        self.HammersmithLabels = self.HammersmithDir.join("hammers_mithLabels.csv", shouldExist=True)
+        self.Hammersmith = self.HammersmithDir.join("Hammers_mith_atlas_n30r83_SPM5.nii.gz", shouldExist=True)
+        self.HammersmithLobarLabels = self.HammersmithDir.join("hammers_mithLobarLabels.csv", shouldExist=True)
+        self.HammersmithLobar = self.HammersmithDir.join("Hammers_mith_atlas_Lobar_SPM5.nii.gz", shouldExist=True)
 
         # PET Masks
         self.cerebellum_whole_eroded = self.masks.join("Cerebellum-MNIflirt_bin_eroded_whole.nii.gz", shouldExist=True)
