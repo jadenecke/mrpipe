22ea69118cb685f38bde9c601dfd8a7456a74443
jadenecke <44135911+jadenecke@users.noreply.github.com>
2026-05-17 19:25:36 +0200

    hdbet fix

--- mrpipe/Toolboxes/standalone/HDBet.py
+++ mrpipe/Toolboxes/standalone/HDBet.py
@@ -6,22 +6,23 @@ class HDBET(Task):
 
     def __init__(self, infile, session, brain, mask, useGPU = False, name: str = "hdbet", verbose=False, clobber=False):
         super().__init__(name=name, clobber=clobber, session=session)
-        self.addInFiles(infile)
         self.inputImage = infile
-        self.addOutFiles([brain, mask])
         self.outputBrain = brain
         self.outputMask = mask
         self.verbose = verbose
         self.useGPU = useGPU
-        self.command = os.path.join(Helper.get_libpath(), "Toolboxes", "submodules", "hdbet", "HD_BET", "hd_bet_cli.py")
+        #self.command = os.path.join(Helper.get_libpath(), "Toolboxes", "submodules", "hdbet", "HD_BET", "hd_bet_cli.py")
+        self.command = "hd-bet"
 
+        self.addInFiles(infile)
+        self.addOutFiles([brain, mask])
 
     def getCommand(self):
-        command = f"{self.command} -i {self.inputImage} -o {self.outputBrain}"
+        command = f"{self.command} -i {self.inputImage} -o {self.outputBrain} --save_bet_mask"
         if not self.useGPU:
-            command += f" -device cpu -tta 0"
+            command += f" -device cpu --disable_tta"
         if self.verbose:
-            command += f" -v"
+            command += f" --verbose"
         return command
 
 
